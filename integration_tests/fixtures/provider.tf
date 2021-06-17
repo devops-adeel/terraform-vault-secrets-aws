570785e01ccb0fcc98fb273b3df06fbbd9d3c167
tfc_run <team-is@hashicorp.com>
2021-06-17 13:56:55 +0100

    Rendered provider configuration

--- integration_tests/fixtures/provider.tf
+++ integration_tests/fixtures/provider.tf
@@ -18,7 +18,7 @@ variable "approle_secret" {}
 
 provider "vault" {
   auth_login {
-    namespace = "admin/terraform-vault-secrets-aws"
+    namespace = "vault_namespace"
     path = "auth/approle/login"
 
     parameters = {
